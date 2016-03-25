77c39248e9751d26bcb95109511561c35e4e5f42
Evi <efremova.vasilina@mail.ru>
2016-03-25 13:18:51 +0200

    optimization functions number

--- number.php
+++ number.php
@@ -1,7 +1,7 @@
 <?php
 
 $number = new Number();
-$number->calc(2500);
+$number->calc(3464);
 
 class Number
 {
@@ -25,72 +25,71 @@ class Number
 
     public function calc($number)
     {
-        if ($number/1000 >= 1){
-            $n = floor($number/1000);
-            for($i = 1; $i <= $n; $i++){
+        // thousands
+        if ($number >= 1000) {
+            $n = floor($number / 1000);
+            for ($i = 1; $i <= $n; $i++) {
                 $this->_result[] = self::THOUSAND;
             }
-            $this->calc($number%1000);
-        }
-        elseif($number/500 >= 1){
-            $this->_result[] = self::FIVE_HUNDRED;
-            $this->calc($number-500);
-        }
-        elseif($number/100 >= 1){
-            $n = floor($number/100);
-            if($n == 4){
-                $this->_result[] = self::FOUR_HUNDRED;
-            }
-            elseif($n == 9){
+            $this->calc($number % 1000);
+
+            // hundreds
+        } elseif ($number / 100 >= 1) {
+            $n = floor($number / 100);
+            if ($n == 9) {
                 $this->_result[] = self::NINE_HUNDRED;
-            }
-            else {
-                for($i = 1; $i <= $n; $i++){
+                $this->calc($number - 900);
+            } elseif ($number / 500 >= 1) {
+                $this->_result[] = self::FIVE_HUNDRED;
+                $this->calc($number - 500);
+            } elseif ($n == 4) {
+                $this->_result[] = self::FOUR_HUNDRED;
+                $this->calc($number - 400);
+            } else {
+                for ($i = 1; $i <= $n; $i++) {
                     $this->_result[] = self::HUNDRED;
                 }
+                $this->calc($number % 100);
             }
-            $this->calc($number%100);
-        }
-        elseif($number/50 >= 1){
-            $this->_result[] = self::FIFTY;
-            $this->calc($number-50);
-        }
-        elseif($number/10 >= 1){
-            $n = floor($number/10);
-            if($n == 4){
-                $this->_result[] = self::FORTY;
-            }
-            elseif($n == 9){
+
+            // dozens
+        } elseif ($number / 10 >= 1) {
+            $n = floor($number / 10);
+            if ($n == 9) {
                 $this->_result[] = self::NINETY;
-            }
-            else {
-                for($i = 1; $i <= $n; $i++){
+                $this->calc($number - 90);
+            } elseif ($number / 50 >= 1) {
+                $this->_result[] = self::FIFTY;
+                $this->calc($number - 50);
+            } elseif ($n == 4) {
+                $this->_result[] = self::FORTY;
+                $this->calc($number - 40);
+            } else {
+                for ($i = 1; $i <= $n; $i++) {
                     $this->_result[] = self::TEN;
                 }
+                $this->calc($number % 10);
             }
-            $this->calc($number%10);
-        }
-        elseif($number >= 0){
 
-            if($number < 4) {
-                for($i = 1; $i <= $number; $i++){
+            // units
+        } elseif ($number >= 0) {
+            if ($number < 4) {
+                for ($i = 1; $i <= $number; $i++) {
                     $this->_result[] = self::ONE;
                 }
-            }
-            elseif($number == 4){
+            } elseif ($number == 4) {
                 $this->_result[] = self::FOUR;
-            }
-            elseif($number > 5 && $number < 9){
+            } elseif ($number > 5 && $number < 9) {
                 $this->_result[] = self::FIVE;
-                for($i = 1; $i <= ($number - 5); $i++){
+                for ($i = 1; $i <= ($number - 5); $i++) {
                     $this->_result[] = self::ONE;
                 }
-            }
-            elseif($number == 9){
+            } elseif ($number == 9) {
                 $this->_result[] = self::NINE;
             }
 
-            echo implode('', $this->_result);
+            // result
+            echo implode('.', $this->_result);
         }
     }
 }
